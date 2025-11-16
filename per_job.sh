a8cb5251beae59c37b616ad94c5e30a777ce4835
changxi <27131403+chauncyzhu@users.noreply.github.com>
2025-11-16 14:38:24 +0100

    update

--- per_job.sh
+++ per_job.sh
@@ -7,11 +7,13 @@
 #SBATCH --distribution=cyclic
 #SBATCH --spread-job
 
-map_name=$1
+python_dir=$1
+map_name=$2
+method_name=$3
 
 for i in `seq 1 $SLURM_NTASKS`; do
-    nohup python src/main.py --alg-config=tgcnet --env-config=sc2 with env_args.map_name="8m_vs_9m" > output"${i}".log 2>&1 &
-    sleep 5
+    nohup $python_dir/python src/main.py --alg-config=$method_name --env-config=sc2 with env_args.map_name=$map_name > output"${i}".log 2>&1 &
+    sleep 2
 done
 wait
 
--- run_slurm.sh
+++ run_slurm.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 map_name="5m_vs_6m"
-sbatch -J $map_name -p "gpu_a100" --gpus-per-node 1 --cpus-per-task 12 per_job.sh $map_name
+method_name="tgcnet"
+python_dir=/scratch/zhu00030/anaconda3/envs/transfer/bin
+
+sbatch -J $map_name -p "gpua100" --gpus-per-node 1 --cpus-per-task 12 per_job.sh $python_dir $map_name $method_name
 
