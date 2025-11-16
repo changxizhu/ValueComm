4ebdd351b4c055d1dd93da2532626c1fb1b000e1
changxi <27131403+chauncyzhu@users.noreply.github.com>
2025-11-16 14:57:14 +0100

    submit

--- run_slurm.sh
+++ run_slurm.sh
@@ -5,15 +5,14 @@ python_dir=/scratch/zhu00030/anaconda3/envs/transfer/bin
 
 # Lists of map names and methods
 # map_names=("2s3z" "3s5z" "3s5z_vs_3s6z")
-# map_names=("3m" "5m_vs_6m" "8m_vs_9m" "10m_vs_11m")
-map_names=("10m_vs_11m")
+map_names=("3m" "5m_vs_6m" "8m_vs_9m" "10m_vs_11m")
 methods=("tgcnet" "maic")
 
 # Loop over all combinations
 for map_name in "${map_names[@]}"; do
     for method_name in "${methods[@]}"; do
         echo "Submitting job: map=$map_name, method=$method_name"
-        sbatch -J "$map_name" -p "gpua100" --gpus-per-node 1 --cpus-per-task 12 per_job.sh "$python_dir" "$map_name" "$method_name"
+        sbatch -J "$map_name" -p "gpua16" --gpus-per-node 1 --cpus-per-task 12 per_job.sh "$python_dir" "$map_name" "$method_name"
     done
 done
 
